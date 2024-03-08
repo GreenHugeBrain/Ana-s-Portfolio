5ca462c8488ab278cf7e74b29692026172dc2d3d
GreenHugeBrain <khvtisozedelashvili@gmail.com>
2024-03-08 21:10:27 +0400

    responsive and scroll

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var headerContainer = document.querySelector('.header-container');
-  var upButton = document.querySelector('.up-button');
+$(document).ready(function () {
+  $('.loading-screen').fadeIn();
 
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 10) {
-      headerContainer.classList.add('scrolled');
-      upButton.classList.add('show-up-button');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        loadContent();
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  observer.observe(document.getElementById('scroll-trigger'));
+
+  function loadContent() {
+    setTimeout(function () {
+      $('.loading-screen').fadeOut();
+      $('.blur-container').addClass('loaded');
+      $('body').css('overflow', 'auto');
+    }, 2000);
+  }
+
+  const headerContainer = $('.header-container');
+  const upButton = $('.up-button');
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 10) {
+      headerContainer.addClass("scrolled");
+      upButton.addClass("show-up-button");
     } else {
-      headerContainer.classList.remove('scrolled');
-      upButton.classList.remove('show-up-button');
+      headerContainer.removeClass("scrolled");
+      upButton.removeClass("show-up-button");
     }
   });
 
-  upButton.addEventListener('click', function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    upButton.classList.remove('show-up-button');
+  upButton.on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    upButton.removeClass("show-up-button");
   });
+});
 
-  var links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
-      var target = document.querySelector(link.getAttribute('href'));
-      if (target) {
-        window.scrollTo({ top: target.offsetTop, behavior: 'smooth' });
-      }
-    });
+
+$(document).ready(function () {
+  $('a[href^="#"]').on('click', function (event) {
+    event.preventDefault();
+
+    var target = $($(this).attr('href'));
+
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
   });
 });
